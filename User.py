8c1e7517afe1a6e2395d5e1ace353c69a7efc11c
shaharBabkoff <155917341+shaharBabkoff@users.noreply.github.com>
2024-02-15 14:12:46 +0200

    Update User.py

--- User.py
+++ User.py
@@ -1,4 +1,4 @@
-import Post
+from Post import PostFactory, PostTypes
 
 
 class User:
@@ -6,25 +6,18 @@ class User:
         self.username = username
         self.password = password
         self.following = []
-        self.observers = []
+        self.followers = []
         self.posts = []
         self.notifications = []
         self.is_online = True
 
+    # in main when we print the object it will know how to print by this method
     def __str__(self):
         return (f"User name: {self.username}, Number of posts: {len(self.posts)}"
-                f", Number of followers: {len(self.observers)}")
+                f", Number of followers: {len(self.followers)}")
 
-    def add_observer(self, observer):
-        self.observers.append(observer)
-
-    def remove_observer(self, observer):
-        self.observers.remove(observer)
-
-    def notify_observers(self, notification):
-        for observer in self.observers:
-            observer.update(self, notification)
-
+    # this method will be used while using "observer" design pattern.
+    # it will update a user and add the notification to his notification list
     def update(self, observable, notification: str):
         self.notifications.append(notification)
 
@@ -40,35 +33,34 @@ class User:
         else:
             pass
 
+    # when a user follow other user, the other user is added to the user following list
+    # the user is added to the other user follower list
     def follow(self, other_user):
         if other_user not in self.following:
             self.following.append(other_user)
-            other_user.add_observer(self)
+            other_user.followers.append(self)
             print(self.username, "started following", other_user.username, "")
 
+    # when a user unfollow other user, the other user is removed from the user following list
+    # the user is removed from the other user follower list
     def unfollow(self, other_user: 'User'):
         if other_user in self.following:
             self.following.remove(other_user)
-            other_user.remove_observer(self)
+            other_user.followers.remove(self)
             print(self.username, "unfollowed", other_user.username, "")
 
+    # in this method we will use "factory" design pattern
+    # here we are actually publishing a post. we don't need to handle here which type is the post of.
+    # This we are doing in PostFactory class
     def publish_post(self, post_type, content, *args):
-            if post_type == "Text":
-                post = Post.TextPost(self, content)
-            elif post_type == "Image":
-                post = Post.ImagePost(self, content)
-            elif post_type == "Sale":
-                post = Post.SalePost(self, content, *args)  # SalePost expects *args for additional info like price
-            else:
-                print(f"Unsupported post type: {post_type}")
-                return None
-            self.posts.append(post)
-            post.add_observer(self)
-            # self.notify_observers(f"{self.username} has a new post")
-            print(post)
+        new_post = PostFactory()
+        post = new_post.post_factory(self, PostTypes(post_type), content, *args)
+        self.posts.append(post)
+        post.add_observer(self)
+        print(post)
+        return post
 
-            return post
-
+    # printing the list of the notifications of the user
     def print_notifications(self):
         if self.notifications:  # Check if there are any notifications
             print(f"{self.username}'s notifications:")
